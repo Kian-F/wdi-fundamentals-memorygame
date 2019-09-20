371101ac07c260b7275cfd3ccb087be2c3f24b95
Kian-F <52603628+Kian-F@users.noreply.github.com>
2019-09-20 16:00:12 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,19 @@
 
-var cardOne = "queen";
-var cardOne = "queen";
-var cardOne = "king";
-var cardOne = "king";
-console.log("Up and running!" + cardOne);
+cards=["queen","queen","king","King"];
+cardsInPlay=[];
+var cardOne;
+cardOne = cards[1];
+cardsInPlay.push(cardOne);
+cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] ===cardsInPlay[1]){
+		alert("You found a match!");
+	}else{alert("Sorry, try again.")}
+	
+}
+
+
+
+console.log("User flipped king");
+console.log("User flipped queen");
